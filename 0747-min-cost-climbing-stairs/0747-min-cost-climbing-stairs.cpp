5dbab65b276911c3bc1766e99a3b5dd447a35aa7
Shubham Patel <138980314+Sp-177@users.noreply.github.com>
2023-10-13 20:38:55 +0530

    Time: 3 ms (84.96%), Space: 14.1 MB (26.23%) - LeetHub

---
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        vector<int>copy=cost;
+        int n=cost.size()-1;
+        for(int i=1,j=2;i<=n,j<=n+1;i++,j++){
+            int x=cost[i-1],y=INT_MAX;
+            if(i-2>=0){
+                y=cost[i-2];
+            }
+            cost[i]+=min(x,y);
+            if(j<=n){
+            int a=copy[j-1],b=INT_MAX;
+            if(j-2>=1){
+                b=copy[j-2];
+            }
+            copy[j]+=min(a,b);
+            }
+        }
+        
+        return min(min(cost[n],cost[n-1]),min(copy[n],copy[n-1]));
+    }
+};
