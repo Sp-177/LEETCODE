a67fdafda0e1d300bc6ff710b819f3f330b9e915
Shubham Patel <138980314+Sp-177@users.noreply.github.com>
2024-02-03 13:11:06 +0530

    Time: 11 ms (74.73%), Space: 36.3 MB (5.14%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    int func(vector<int>&arr,int k,int i,vector<int>&dp){
-        if(i>=arr.size())return 0;
-        if(dp[i]!=-1)return dp[i];
-        int ans=0;
-        int M=arr[i];
-        for(int j=i;j<i+k&&j<arr.size();j++){
-            M=max(M,arr[j]);
-            ans=max(ans,M*(j-i+1)+func(arr,k,j+1,dp));
+    int func(vector<int>&arr,int k,int i,int j,vector<vector<int>>&dp){
+        if(i>j)return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        int ans=INT_MIN;
+        int x=arr[i];
+        for(int l=i;l<i+k&&l<=j;l++){
+            x=max(arr[l],x);
+            ans=max(ans,x*(l-i+1)+func(arr,k,l+1,j,dp));
         }
-        return dp[i]=ans;
-    } 
+        return dp[i][j]=ans;
+    }
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
-        vector<int>dp(arr.size(),-1);
-        return func(arr,k,0,dp);
+        vector<vector<int>>dp(arr.size(),vector<int>(arr.size(),-1));
+        return func(arr,k,0,arr.size()-1,dp);
     }
 };
