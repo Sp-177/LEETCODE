6bfcaae35a8856c596bba9ccae8cac1883e22711
Shubham Patel <138980314+Sp-177@users.noreply.github.com>
2024-05-02 13:14:43 +0530

    Time: 8 ms (12.55%), Space: 44.7 MB (85.18%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,39 +1,48 @@
+import java.util.*;
+
 class Solution {
-public:
-    bool bfs(queue<int>&q,vector<vector<int>> &edges,vector<int>&colored,int n){
-		while(!q.empty()){
-		int node=q.front();
-		q.pop();
-		for(int i:edges[node]){
-				if(colored[i]==abs(colored[node]-1)||colored[i]==-1){
-					if(colored[i]==-1){
-						colored[i]=abs(colored[node]-1);
-						q.push(i);
-					}
-				}
-				else return false;
-			
-		}
-	}
-	return true;
-}
-bool isBipartite(vector<vector<int>> &edges) {
-	// Write your code here.
-	int n=edges.size();
-	vector<int>colored(n,-1);
-	
-	bool ans=true;
-        for (int i = 0; i < n; i++) {
-		queue<int>q;
-                if (colored[i] == -1) {
-                        q.push(i);
-                        colored[i] = 0;
-                        ans &= bfs(q, edges, colored, n);
+    public boolean bfs(Queue<Integer> q, List<List<Integer>> edges, int[] colored, int n) {
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            for (int i : edges.get(node)) {
+                if (colored[i] == Math.abs(colored[node] - 1) || colored[i] == -1) {
+                    if (colored[i] == -1) {
+                        colored[i] = Math.abs(colored[node] - 1);
+                        q.add(i);
+                    }
+                } else {
+                    return false;
                 }
-                if (!ans)
-                        return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isBipartite(int[][] edges) {
+        int n = edges.length;
+        List<List<Integer>> adjList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adjList.add(new ArrayList<>());
+            for (int j = 0; j < edges[i].length; j++) {
+                adjList.get(i).add(edges[i][j]);
+            }
         }
 
-    return ans;
+        int[] colored = new int[n];
+        Arrays.fill(colored, -1);
+        
+        boolean ans = true;
+        for (int i = 0; i < n; i++) {
+            Queue<Integer> q = new LinkedList<>();
+            if (colored[i] == -1) {
+                q.add(i);
+                colored[i] = 0;
+                ans &= bfs(q, adjList, colored, n);
+            }
+            if (!ans) {
+                return false;
+            }
+        }
+        return ans;
+    }
 }
-};
