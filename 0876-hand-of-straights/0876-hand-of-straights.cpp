d439d08bcb08cbd6bc0de4ea0cb41586b697d007
Shubham Patel <138980314+Sp-177@users.noreply.github.com>
2024-06-06 10:36:34 +0530

    Time: 67 ms (35.03%), Space: 30.7 MB (54.6%) - LeetHub

---
+++ 0876-hand-of-straights/0876-hand-of-straights.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        // cout<<hand.size()<<endl;
+        if(hand.size()%groupSize!=0){return false;}
+        map<int,int>m;
+        for(int i :hand){
+            m[i]++;
+        }
+        int l=hand.size()/groupSize;
+        // for(auto j:m){cout<<j.first<<' '<<j.second<<endl;}
+        for(int i=0;i<(hand.size()/groupSize);i++){
+            int it=m.begin()->first;
+            cout<<endl<<it<<endl;
+            int len=0;
+            if(m.size()<groupSize){return false;}
+            for(auto j:m){
+                if(len==groupSize){break;}
+                
+                if(len>0){
+                    if(j.first!=it+1){
+                        return false;
+                    }
+                    it=j.first;
+                }
+                m[j.first]--;
+                if(m[j.first]==0){
+                    m.erase(j.first);
+                }
+                len++;
+            }l--;
+        }
+        return (l==0?true:false);
+    }
+};
