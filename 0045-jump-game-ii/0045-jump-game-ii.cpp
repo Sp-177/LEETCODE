2397d6055c202dc259c63cda2a2c84cbed54cbb8
Shubham Patel <138980314+Sp-177@users.noreply.github.com>
2024-07-11 00:15:47 +0530

    Time: 8 ms (81.53%), Space: 18.9 MB (62.4%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        vector<int>dp(nums.size(),0);
-        int n=nums.size();
-        for(int i=nums.size()-2;i>=0;i--){
-            int jump=INT_MAX-1;
-            for(int j=min(n-1,nums[i]+i);j>i;j--){
-                // cout<<j<<endl;
-                jump=min(jump,dp[j]);
-                // cout<<jump<<",";
-            }
-            //  cout<<jump<<endl;
-             
-            dp[i]=jump+1;
+        if (nums.size() <= 1) return 0;
+        
+        for (int i = 1; i < nums.size(); ++i) {
+            nums[i] = max(nums[i] + i, nums[i - 1]);
         }
-        return dp[0];
+        
+        int currentIndex = 0;
+        int jumps = 0;
+        
+        while (currentIndex < nums.size() - 1) {
+            ++jumps;
+            currentIndex = nums[currentIndex];
+        }
+        
+        return jumps;
     }
 };
