0d4fe19a2dc28ea4c73e5977c19da054d1dc234c
Shubham Patel <138980314+Sp-177@users.noreply.github.com>
2024-10-30 19:36:46 +0530

    Time: 22 ms (42.43%), Space: 11.4 MB (13.35%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -12,21 +12,23 @@ bool check(map<char,int>&m,map<char,int>&n){
         map<char,int>ma,new_ma;
         for(char c:t)ma[c]++;
         int start=0,end=0;
-        bool ans=s!=t?false:true;
-        int l=0,r=n-1;
+        int l=0,len=INT_MAX;
+        int cnt=0;
         while(start<n&&end<n){
             // cout<<start<<' '<<end<<endl;
-            new_ma[s[end++]]++;
-            while(check(ma,new_ma)&&start<end){
-                if(r-l+1>=end-start){
-                    r=end-1;
+            if(ma[s[end]]>0)cnt++;
+            ma[s[end]]--;
+            while(cnt==m){
+                if(len>=end-start+1){
+                    len=end-start+1;
                     l=start;
-                    ans=true;
                 }
-                new_ma[s[start++]]--;
+                ma[s[start]]++;
+                if(ma[s[start++]]>0)cnt--;
+                
             }
-    
+            end++;
         }
-        return ans?s.substr(l,r-l+1):"";
+        return len==INT_MAX?"":s.substr(l,len);
     }
 };
