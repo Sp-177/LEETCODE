7d26ad45484395fac7defd6c784a3c1588c3771d
Shubham Patel <138980314+Sp-177@users.noreply.github.com>
2024-11-03 16:35:29 +0530

    Time: 0 ms (100%), Space: 10 MB (41.17%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -2,13 +2,16 @@ class Solution {
 public:
 
     int numberOfSubstrings(string s) {
-    map<char,int>m;int ans=0,n=s.size(),l=0;
+    int a=0,b=0,c=0;int ans=0,n=s.size(),l=0;
     for(int i=0;i<n;i++){
-        m[s[i]]++;
-        while(m.size()==3){
+        if(s[i]=='a')a++;
+        else if(s[i]=='b')b++;
+        else c++;
+        while(a&&b&&c){
             ans+=n-i;
-            m[s[l]]--;
-            if(m[s[l]]==0)m.erase(s[l]);
+             if(s[l]=='a')a--;
+        else if(s[l]=='b')b--;
+        else c--;
             l++;
         }
         }    
