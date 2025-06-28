15db0c76d9971ad40c97c5225c52cdf1b2ab7022
Shubham Patel <138980314+Sp-177@users.noreply.github.com>
2025-06-29 02:57:08 +0530

    Time: 1882 ms (13.42%), Space: 430 MB (26.43%) - LeetHub

---
+++ 1129-longest-string-chain/1129-longest-string-chain.cpp
@@ -0,0 +1,76 @@
+class Solution {
+public:
+    vector<int> dp;
+
+    class Node {
+    public:
+        int ind;
+        bool isEnd;
+        vector<Node*> child;
+
+        Node() : ind(-1), isEnd(false), child(26, nullptr) {}
+    };
+
+    class Trie {
+    public:
+        Node* root;
+        Trie() { root = new Node(); }
+
+        void insert(string& str, int index) {
+            Node* temp = root;
+            for (char c : str) {
+                if (temp->child[c - 'a'] == nullptr)
+                    temp->child[c - 'a'] = new Node();
+                temp = temp->child[c - 'a'];
+            }
+            temp->isEnd = true;
+            temp->ind = index;
+        }
+
+        int search(string& str) {
+            Node* temp = root;
+            for (char c : str) {
+                if (temp->child[c - 'a'] == nullptr) return -1;
+                temp = temp->child[c - 'a'];
+            }
+            return temp->ind;
+        }
+    };
+
+    int func(vector<string>& words, int n, Trie* obj, int index) {
+        if (index == n) return 0;
+        if (dp[index] != -1) return dp[index];
+
+        int len = words[index].size();
+        string str = words[index];
+        int ans = 1;
+
+        for (char c = 'a'; c <= 'z'; c++) {
+            for (int i = 0; i <= len; i++) {
+                string temp = str.substr(0, i) + c + str.substr(i);
+                int ind = obj->search(temp);
+                if (ind != -1) {
+                    ans = max(ans, func(words, n, obj, ind) + 1);
+                }
+            }
+        }
+        return dp[index] = ans;
+    }
+
+    int longestStrChain(vector<string>& words) {
+        Trie* obj = new Trie();
+        sort(words.begin(), words.end());
+        int n = words.size();
+
+        for (int i = 0; i < n; i++) obj->insert(words[i], i);
+
+        dp.resize(n, -1);
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            ans = max(ans, func(words, n, obj, i));
+        }
+
+        return ans;
+    }
+};
