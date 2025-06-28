350772b032bdae9761eaf86c70258142cf98be36
Shubham Patel <138980314+Sp-177@users.noreply.github.com>
2025-06-29 03:01:42 +0530

    Time: 111 ms (39.82%), Space: 55.8 MB (37.08%) - LeetHub

--- 1129-longest-string-chain/1129-longest-string-chain.cpp
+++ 1129-longest-string-chain/1129-longest-string-chain.cpp
@@ -1,76 +1,23 @@
 class Solution {
 public:
-    vector<int> dp;
-    unordered_map<string,int>mp;
-    class Node {
-    public:
-        int ind;
-        bool isEnd;
-        vector<Node*> child;
-
-        Node() : ind(-1), isEnd(false), child(26, nullptr) {}
-    };
-
-    class Trie {
-    public:
-        Node* root;
-        Trie() { root = new Node(); }
-
-        void insert(string& str, int index) {
-            Node* temp = root;
-            for (char c : str) {
-                if (temp->child[c - 'a'] == nullptr)
-                    temp->child[c - 'a'] = new Node();
-                temp = temp->child[c - 'a'];
-            }
-            temp->isEnd = true;
-            temp->ind = index;
-        }
-
-        int search(string& str) {
-            Node* temp = root;
-            for (char c : str) {
-                if (temp->child[c - 'a'] == nullptr) return -1;
-                temp = temp->child[c - 'a'];
-            }
-            return temp->ind;
-        }
-    };
-
-    int func(vector<string>& words, int n, int index) {
-        if (index == n) return 0;
-        if (dp[index] != -1) return dp[index];
-
-        int len = words[index].size();
-        string str = words[index];
-        int ans = 1;
-
-        for (char c = 'a'; c <= 'z'; c++) {
-            for (int i = 0; i <= len; i++) {
-                string temp = str.substr(0, i) + c + str.substr(i);
-                
-                if (mp.count(temp)) {
-                    ans = max(ans, func(words, n, mp[temp]) + 1);
-                }
-            }
-        }
-        return dp[index] = ans;
-    }
-
     int longestStrChain(vector<string>& words) {
-        // Trie* obj = new Trie();
-        sort(words.begin(), words.end());
-        int n = words.size();
+        unordered_map<string, int> dp;
+        sort(words.begin(), words.end(), [](const string& a, const string& b) {
+            return a.size() < b.size();
+        });
 
-        for (int i = 0; i < n; i++) mp[words[i]]=i;
+        int maxLen = 1;
 
-        dp.resize(n, -1);
-        int ans = 0;
-
-        for (int i = 0; i < n; i++) {
-            ans = max(ans, func(words, n, i));
+        for (const string& word : words) {
+            int best = 0;
+            for (int i = 0; i < word.size(); ++i) {
+                string prev = word.substr(0, i) + word.substr(i + 1);
+                best = max(best, dp[prev]);
+            }
+            dp[word] = best + 1;
+            maxLen = max(maxLen, dp[word]);
         }
 
-        return ans;
+        return maxLen;
     }
 };
