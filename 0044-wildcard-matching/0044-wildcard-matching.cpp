b9614889e2b1621262091780f5dbae79d6f094c1
Shubham Patel <138980314+Sp-177@users.noreply.github.com>
2025-07-03 01:04:15 +0530

    Time: 39 ms (23.13%), Space: 32.6 MB (5.27%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,28 +1,38 @@
 class Solution {
 public:
-    bool solve(int i,int j,string &s,string &p,vector<vector<int>>& dp){
-        if(i<0 and j<0) return true;
-        if(i>=0 and j<0) return false;
-        if(i<0 and j>=0){
-            for(int k=0;k<=j;k++){
-                if(p[k]!='*') return false;
-            }
-            return true;
+    vector<vector<int>>dp;
+    bool func(string &s,string &p,int i,int j,int n,int m){
+        if(i>=n||j>=m){
+        if(i==n&&j==m)return true;
+        return false;
         }
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]==p[j] or p[j]=='?'){
-            return dp[i][j] = solve(i-1,j-1,s,p,dp);
+        if(dp[i][j]!=-1)return dp[i][j];
+        if(p[j]=='*'){
+            return dp[i][j]=(func(s,p,i+1,j,n,m)||func(s,p,i+1,j+1,n,m)||func(s,p,i,j+1,n,m));
         }
-        else if(p[j]=='*'){
-            return dp[i][j] = (solve(i-1,j,s,p,dp) or solve(i,j-1,s,p,dp));
-        }
-        else{
-            return false;
-        }
-    }
+        else if (p[j]=='?'||s[i]==p[j])return dp[i][j]=func(s,p,i+1,j+1,n,m);
+        
+        return dp[i][j]=false;
+
+    } 
     bool isMatch(string s, string p) {
-        int i=s.size()-1,j=p.size()-1;
-        vector<vector<int>> dp(s.size(),vector<int>(p.size(),-1));
-        return solve(i,j,s,p,dp);
+        s=s+" ";
+        p=p+" ";
+        int n=s.size(),m=p.size();
+        dp.resize(n+1,vector<int>(m+1,0));
+        dp[n][m]=true;
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                 if(p[j]=='*'){
+            dp[i][j]=(dp[i+1][j]|dp[i+1][j+1]||dp[i][j+1]);
+        }
+            else if (p[j]=='?'||s[i]==p[j])dp[i][j]=dp[i+1][j+1];
+        
+            else dp[i][j]=false;
+
+            }
+        }
+
+        return dp[0][0];
     }
 };
