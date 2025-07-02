47d66f1ffcc96e749d79fa3f06fad357e427e534
Shubham Patel <138980314+Sp-177@users.noreply.github.com>
2025-07-03 01:53:31 +0530

    Time: 12 ms (22.78%), Space: 13.4 MB (24.96%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -15,8 +15,20 @@ vector<vector<int>>dp;
     }
     int minDistance(string w1, string w2) {
         int n=w1.size(),m=w2.size();
-        if(n==0)return m;
-        dp.resize(n,vector<int>(m,-1));
-        return func(w1,w2,0,0,n,m);
+        // if(n==0)return m;
+        dp.resize(n+1,vector<int>(m+1,INT_MAX-1));
+        for(int i=0;i<=n;i++)dp[i][m]=n-i;
+        for(int i=0;i<=m;i++)dp[n][i]=m-i;
+        // return func(w1,w2,0,0,n,m);
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                int ans=INT_MAX-1;
+                if(w1[i]==w2[j]){
+                 ans=dp[i+1][j+1];
+                }
+                 dp[i][j]=min({ans,dp[i+1][j+1]+1,dp[i+1][j]+1,dp[i][j+1]+1});
+            }
+        }
+        return dp[0][0];
     }
 };
