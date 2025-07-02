067fe1d3d656638b7fdb01cb0ceb4edc8d7e0213
Shubham Patel <138980314+Sp-177@users.noreply.github.com>
2025-07-03 00:02:10 +0530

    Time: 109 ms (34.29%), Space: 100.5 MB (11.97%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
-int func(vector<int>&prices,int i,int j,int fee,vector<vector<int>>&dp){
-	
-	if(i>=prices.size())return 0;
-	if(dp[i][j]!=-1)return dp[i][j];
-	if(j==1){
-		return dp[i][j]=max(-prices[i]+func(prices,i+1,0,fee,dp),func(prices,i+1,1,fee,dp));
-	}
-	else return dp[i][j]=max(prices[i]+func(prices,i+1,1,fee,dp)-fee,func(prices,i+1,0,fee,dp));
+    int F;
+    vector<vector<int>>dp;
+    int func(vector<int>&prices,int n,int index,int mode){
+        if(index>=n)return 0;
+        if(dp[index][mode]!=-1)return dp[index][mode];
+        if(mode==1){
+            return dp[index][mode]=max(func(prices,n,index+1,mode^1)-prices[index],func(prices,n,index+1,mode));
+        }
+        return dp[index][mode]=max(func(prices,n,index+1,mode^1)+prices[index]-F,func(prices,n,index+1,mode));
 
-}
+    } 
     int maxProfit(vector<int>& prices, int fee) {
-    vector<vector<int>>dp(prices.size(),vector<int>(2,-1));
-	return func(prices,0,1,fee,dp);
+        F=fee;
+        int n=prices.size();
+        dp.resize(n,vector<int>(2,-1));
+        return func(prices,n,0,1);
     }
 };
