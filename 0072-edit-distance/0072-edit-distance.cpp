ff11e906cff7a2828c45fa84b156ee90224858bc
Shubham Patel <138980314+Sp-177@users.noreply.github.com>
2025-07-03 01:44:33 +0530

    Time: 24 ms (18.2%), Space: 14.2 MB (20.17%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
-    int minDistance(string word1, string word2) {
-    vector<vector<int>>dp(word1.size()+1,vector<int>(word2.size()+1,-1));
-    //return func(word1,word1,word1.size(),word1.size(),dp)-1;
-    for(int i=0;i<=word1.size();i++)dp[i][0]=i;
-    for(int i=0;i<=word2.size();i++)dp[0][i]=i;
-    for(int i=1;i<=word1.size();i++){
-        for(int j=1;j<=word2.size();j++){
-             if(word1[i-1]==word2[j-1]){
-                dp[i][j]=dp[i-1][j-1];
-                }
-            else dp[i][j]=1+min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]));
+vector<vector<int>>dp;
+    int func(string&w,string &v,int i,int j,int n,int m){
+        // cout<<i<<" "<<j<<endl;
+        if(j==m)return n-i;
+        if(i==n)return m-j;
+        if(dp[i][j]!=-1)return dp[i][j];
+        int ans=INT_MAX-1;
+        if(w[i]==v[j]){
+            ans=func(w,v,i+1,j+1,n,m);
         }
+        return dp[i][j]=min({ans,func(w,v,i+1,j+1,n,m)+1,func(w,v,i+1,j,n,m)+1,func(w,v,i,j+1,n,m)+1});
+
     }
-    return dp[word1.size()][word2.size()];
+    int minDistance(string w1, string w2) {
+        int n=w1.size(),m=w2.size();
+        if(n==0)return m;
+        dp.resize(n,vector<int>(m,-1));
+        return func(w1,w2,0,0,n,m);
     }
 };
