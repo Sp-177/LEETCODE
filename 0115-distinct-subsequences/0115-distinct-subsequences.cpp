e4710d2e9e2b3a0b808cf237c8f793efa9dcbb9d
Shubham Patel <138980314+Sp-177@users.noreply.github.com>
2025-07-03 11:55:02 +0530

    Time: 35 ms (34.85%), Space: 26.9 MB (63.43%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
-    int numDistinct(string s, string t) {
-        long long m=1e9+7;
-        vector<vector<long long int>>dp(s.size()+1,vector<long long int>(t.size()+1));
-        for(int i=0;i<s.size();i++)dp[i][0]=1;
-        for(int i=1;i<=s.size();i++){
-            for(int j=1;j<=t.size();j++){
-                long long int ta=0;
-                long long int n_t=dp[i-1][j];
-                if(s[i-1]==t[j-1]){
-                    ta=dp[i-1][j-1];
-                }
-                dp[i][j]=(ta+n_t)%m;
-            }
-        }
-        return dp[s.size()][t.size()];
+    vector<vector<int>>dp;
+    int func(string&s,string&t,int i,int j,int n,int m){
+        if(j==m)return 1;
+        if(i>=n)return 0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        int ta=0,nt=0;
+        if(s[i]==t[j])ta=func(s,t,i+1,j+1,n,m);
+        nt=func(s,t,i+1,j,n,m);
+        return dp[i][j]=ta+nt;
     }
-
+    int numDistinct(string s, string t) {
+        int n=s.size(),m=t.size();
+        dp.resize(n,vector<int>(m,-1));
+        return func(s,t,0,0,n,m);
+    }
 };
