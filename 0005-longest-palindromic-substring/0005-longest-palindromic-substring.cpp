21e874ddce436230192204e5e74ec083f9c12ab6
Shubham Patel <138980314+Sp-177@users.noreply.github.com>
2025-07-04 02:19:56 +0530

    Time: 259 ms (20.65%), Space: 11.2 MB (62.18%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,28 +1,40 @@
 class Solution {
 public:
-     string helper(string &s,int start,int end){
-        string out="";
-        int n=s.size();
-        while(start>=0&&end<n&&s[start]==s[end]){
-                
-                out+=s[end];
-                if(start!=end)out=s[start]+out;
-                start--;end++;
-            }
-        return out;
-     }
+    string pp(string&s){
+        string t="^";
+        for(auto i:s){
+            t+="#";
+            t+=i;
+        }
+        t+="#$";
+        return t;
+    }
     string longestPalindrome(string s) {
-        int n=s.size();
+        string temp=pp(s);
+        int n=temp.size();
+        vector<int>p(n,0);
+        int c=0,r=0;
+        for(int i=1;i<n-1;i++){
+            int m=2*c-i;
+            if(i<r){
+                p[i]=min(r-i,p[m]);
+            }
+            while(temp[i+p[i]+1]==temp[i-p[i]-1])p[i]++;
+            
+            if(i+p[i]>r)c=i,r=i+p[i];
+        }
+        int maxi=*max_element(p.begin(),p.end());
         string ans="";
+        for(auto i:p)cout<<i<<" ";
+        cout<<endl;
         for(int i=0;i<n;i++){
-            
-            string out=helper(s,i,i);
-            if(out.size()>=ans.size()){ans=out;}
-            string out1=helper(s,i,i+1);
-            if(out1.size()>=ans.size()){ans=out1;}
-
-            
+            if(maxi==p[i]){
+                int start=(i-maxi)/2;
+                ans=s.substr(start,maxi);
+                break;
+            }
         }
         return ans;
+
     }
 };
