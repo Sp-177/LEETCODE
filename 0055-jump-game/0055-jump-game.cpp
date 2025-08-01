29850ac5eaa6e0034bed1f8fd0fbf3236f5a2103
Shubham Patel <138980314+Sp-177@users.noreply.github.com>
2025-08-01 23:29:05 +0530

    Time: 0 ms (100%), Space: 52.3 MB (64.06%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-    int last_index=nums.size()-1;
-    bool flag=true;
-        for(int i=last_index-1;i>=0;i--){
-            if(nums[i]>=last_index-i){last_index=i;flag=true;}
-            else{flag=false;}
+        bool can=true;
+        int n=nums.size();
+        int index=-1;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]==0&&can){
+                can=false;
+                index=i;
+            }
+            else {
+                if(i+nums[i]>index)can=true;
+            }
         }
-        return flag;
+        return can;
     }
 };
