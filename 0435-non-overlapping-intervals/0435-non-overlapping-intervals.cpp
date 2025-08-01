244903c49c3a1cb8a730dc62a9dd819395605cfc
Shubham Patel <138980314+Sp-177@users.noreply.github.com>
2025-08-01 18:59:22 +0530

    Time: 49 ms (48.13%), Space: 93.8 MB (87.94%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    static bool cmp(vector<int>&a,vector<int>&b){
-        if(a[1]<b[1]){return true;}
-        else if(a[1]==b[1]){
-            return a[0]<b[0];
-        }
-        return false;
-    }   
-    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        sort(intervals.begin(),intervals.end(),cmp);
-        // for(auto i:intervals){cout<<i[0]<<' '<<i[1]<<endl;}
-        int ans=0,end=intervals[0][1];
-        for(int i=1;i<intervals.size();i++){
-            if(intervals[i][0]<end){ans++;}
-            else{
-                end=intervals[i][1];
+    int eraseOverlapIntervals(vector<vector<int>>& inv) {
+        auto cmp=[&](vector<int>&a,vector<int>&b)->bool{
+            if(a[1]<b[1])return true;
+            else if(a[1]==b[1])return a[0]<b[0];
+            return false;
+        };
+        sort(inv.begin(),inv.end(),cmp);
+        int start=inv[0][0],end=inv[0][1];
+        int cnt=0;
+        int n=inv.size();
+        // for(auto i:inv)cout<<i[0]<<" "<<i[1]<<endl;
+        for(int i=1;i<n;i++){
+            int s=inv[i][0],e=inv[i][1];
+            if(!(end<=s&&end<e)){
+                cnt++;
+                continue;
             }
+            start=s;end=e;
         }
-        return ans;
-
+        return cnt;
     }
 };
