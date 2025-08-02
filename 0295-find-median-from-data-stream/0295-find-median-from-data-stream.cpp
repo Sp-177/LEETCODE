2250a2ae8390c7aa686069a3ab04f1cd4dbcf80e
Shubham Patel <138980314+Sp-177@users.noreply.github.com>
2025-08-03 02:37:54 +0530

    Time: 101 ms (8.5%), Space: 142.4 MB (5.01%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,55 +1,34 @@
 class MedianFinder {
-public:
-    priority_queue<double>mini;
-    priority_queue<double,vector<double>,greater<double>>maxi;
-    double mid=INT_MIN;
-    MedianFinder() {
-        
-    }
-    
-    void addNum(int num) {
-        int leftsize=mini.size();
-        int rightsize=maxi.size();
-        if(mid==INT_MIN){mid=num;}
-        else{
-            if(num>mid){
-                maxi.push(num);
-            }
-            else{
-                mini.push(num);
-            }
-            
-                if(mini.size()>maxi.size() &&(mini.size()-maxi.size()>1)){
-                    maxi.push(mid);
-                    mid=mini.top();
-                    mini.pop();
-                }
-                else if(maxi.size()>mini.size() &&(maxi.size()-mini.size()>1)){
-                    mini.push(mid);
-                    mid=maxi.top();
-                    maxi.pop();
-                }
-            
-        }
-        // cout<<mini.size()<<' '<<maxi.size();
+private:
+    multiset<int> low, high;
 
-    }
-    
-    double findMedian() {
-        int leftsize=mini.size();
-        int rightsize=maxi.size();
-        int len=leftsize+rightsize+1;
-        // cout<<len<<endl;
-        if(len&1){
-            return mid;
+public:
+    MedianFinder() {}
+
+    void addNum(int num) {
+        if (low.empty() || num <= *low.rbegin()) {
+            low.insert(num);
+        } else {
+            high.insert(num);
         }
-        return (mid+(leftsize>rightsize?mini.top():maxi.top()))/2.0;
+
+        // Balance sizes
+        if (low.size() > high.size() + 1) {
+            high.insert(*low.rbegin());
+            auto it = low.end();
+            --it;
+            low.erase(it);
+        } else if (high.size() > low.size()) {
+            low.insert(*high.begin());
+            high.erase(high.begin());
+        }
+    }
+
+    double findMedian() {
+        if (low.size() == high.size()) {
+            return (*low.rbegin() + *high.begin()) / 2.0;
+        } else {
+            return *low.rbegin();
+        }
     }
 };
-
-/**
- * Your MedianFinder object will be instantiated and called as such:
- * MedianFinder* obj = new MedianFinder();
- * obj->addNum(num);
- * double param_2 = obj->findMedian();
- */
