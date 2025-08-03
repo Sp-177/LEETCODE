0be632b68e5fce92384c0b0fa6c476d6807d701f
Shubham Patel <138980314+Sp-177@users.noreply.github.com>
2025-08-03 21:31:43 +0530

    Time: 145 ms (56.49%), Space: 72.7 MB (57.65%) - LeetHub

---
+++ 1422-divide-array-in-sets-of-k-consecutive-numbers/1422-divide-array-in-sets-of-k-consecutive-numbers.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPossibleDivide(vector<int>& hand, int groupSize) {
+         int n = hand.size();
+        if (n % groupSize != 0) return false;
+
+        map<int, int> freq;
+        for (int card : hand) {
+            freq[card]++;
+        }
+
+        while (!freq.empty()) {
+            int start = freq.begin()->first;  // smallest available card
+            for (int i = 0; i < groupSize; ++i) {
+                int card = start + i;
+                if (freq[card] == 0) return false;
+                freq[card]--;
+                if (freq[card] == 0) freq.erase(card);
+            }
+        }
+
+        return true;
+    }
+};
