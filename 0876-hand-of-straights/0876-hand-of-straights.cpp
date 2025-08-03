2aa06acb420937fc776562187412f2f4cf7ccda3
Shubham Patel <138980314+Sp-177@users.noreply.github.com>
2025-08-03 17:41:54 +0530

    Time: 35 ms (69.61%), Space: 32.3 MB (38.64%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.cpp
+++ 0876-hand-of-straights/0876-hand-of-straights.cpp
@@ -1,35 +1,24 @@
 class Solution {
 public:
-    bool isNStraightHand(vector<int>& hand, int groupSize) {
-        // cout<<hand.size()<<endl;
-        if(hand.size()%groupSize!=0){return false;}
-        map<int,int>m;
-        for(int i :hand){
-            m[i]++;
+    bool isNStraightHand(vector<int>& hand, int gS) {
+        int n=hand.size();
+        if(n%gS!=0)return 0;
+        map<int,int>freq;
+        for(int i:hand)freq[i]++;
+        int tg=n/gS;
+        for(int i=0;i<tg;i++){
+            int cnt=0,last=-1;
+            for(auto i:freq){
+                if(cnt==gS)break;
+                int curr=i.first;
+                if(last!=-1&& (curr-last)!=1)return false;
+                freq[curr]--;
+                if(freq[curr]==0)freq.erase(curr);
+                cnt++;
+                last=curr;
+            }
+            if(cnt!=gS)return false;
         }
-        int l=hand.size()/groupSize;
-        // for(auto j:m){cout<<j.first<<' '<<j.second<<endl;}
-        for(int i=0;i<(hand.size()/groupSize);i++){
-            int it=m.begin()->first;
-            // cout<<endl<<it<<endl;
-            int len=0;
-            if(m.size()<groupSize){return false;}
-            for(auto j:m){
-                if(len==groupSize){break;}
-                
-                if(len>0){
-                    if(j.first!=it+1){
-                        return false;
-                    }
-                    it=j.first;
-                }
-                m[j.first]--;
-                if(m[j.first]==0){
-                    m.erase(j.first);
-                }
-                len++;
-            }l--;
-        }
-        return (l==0?true:false);
+        return true;
     }
 };
