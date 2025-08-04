909d61e5a9fbe701e986e18618e1e5b1c9ddcff8
Shubham Patel <138980314+Sp-177@users.noreply.github.com>
2025-08-04 18:02:27 +0530

    Time: 543 ms (31.22%), Space: 240.2 MB (68.53%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.cpp
+++ 0493-reverse-pairs/0493-reverse-pairs.cpp
@@ -1,31 +1,45 @@
 class Solution {
 public:
-    long long  divide(vector<int>&nums,long long  low,long long  high){
-        if(high<=low)return 0;
-        long long  mid=(low+high)/2;
-        long long  a1=divide(nums,low,mid);
-        long long  a2=divide(nums,mid+1,high);
-        long long  a3=merge(nums,low,mid,high);
-        // cout<<"A-"<<a1+a2+a3<<endl;
-        return a1+a2+a3;
-    }
-    long long  merge(vector<int>&nums,long long  low,long long  mid,long long  high){
-        // cout<<low<<' '<<mid<<' '<<high<<endl;
-        sort(nums.begin()+low,nums.begin()+mid+1);
-        // for(long long  i=0;i<=mid;i++)cout<<nums[i]<<" ";
-        // cout<<endl;
-        long long  ans=0;
-        for(long long  i=mid+1;i<=high;i++){
-            // cout<<nums[i]<<' ';
-            long long  index=upper_bound(nums.begin()+low,nums.begin()+mid,2*1LL*nums[i])-nums.begin()-low;
-            if(index+low<=mid&&nums[index+low]>2*1LL*nums[i]){
-                ans+=(mid-index-low+1);
+    int ans = 0;
+
+    void merge(vector<int>& nums, int s, int mid, int e) {
+        int cnt = 0;
+        for (int i = mid + 1; i <= e; i++) {
+            auto it = lower_bound(nums.begin() + s, nums.begin() + mid + 1, 2LL * nums[i] + 1);
+            if (it != nums.begin() + mid + 1) {
+                int ind = it - nums.begin();
+                cnt += (mid - ind + 1);
             }
         }
-        // cout<<endl;
-        return ans;
+
+        ans += cnt;
+
+        vector<int> merger;
+        int i = s, j = mid + 1;
+        while (i <= mid && j <= e) {
+            if (nums[i] < nums[j]) {
+                merger.push_back(nums[i++]);
+            } else {
+                merger.push_back(nums[j++]);
+            }
+        }
+        while (i <= mid) merger.push_back(nums[i++]);
+        while (j <= e) merger.push_back(nums[j++]);
+
+        for (int k = 0; k < merger.size(); ++k)
+            nums[s + k] = merger[k];
     }
-    long long  reversePairs(vector<int>& nums) {
-        return divide(nums,0,nums.size()-1);
+
+    void dnc(vector<int>& nums, int s, int e) {
+        if (s >= e) return;
+        int mid = (s + e) / 2;
+        dnc(nums, s, mid);
+        dnc(nums, mid + 1, e);
+        merge(nums, s, mid, e);
+    }
+
+    int reversePairs(vector<int>& nums) {
+        dnc(nums, 0, nums.size() - 1);
+        return ans;
     }
 };
