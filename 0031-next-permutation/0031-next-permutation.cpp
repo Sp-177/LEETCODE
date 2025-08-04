c4b7243610b555b1fbd7050b8a96c23aba08296e
Shubham Patel <138980314+Sp-177@users.noreply.github.com>
2025-08-05 01:05:03 +0530

    Time: 0 ms (100%), Space: 15.6 MB (67.43%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,31 +1,19 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& A) {
-         bool f=false;
-    int index=0;
-    for(int i=A.size()-1;i>=1;i--){
-      if(A[i]>A[i-1]){
-          index=i-1;
-          f=true;
-          break;
-      }
+    void nextPermutation(vector<int>& nums) {
+        int n=nums.size();
+        int i=-1;
+        bool swapped=false;
+        for(i=n-1;i>=1;i--){
+            if(nums[i-1]<nums[i]){
+                int mini=i;
+                for(int j=i;j<n;j++)if(nums[j]>nums[i-1]&&nums[j]<nums[mini])mini=j;
+                swap(nums[i-1],nums[mini]);
+                sort(nums.begin()+i,nums.end());
+                swapped=true;
+                break;
+            }
+        }
+        if(!swapped)sort(nums.begin(),nums.end());
     }
-    // cout<<index<<endl;
-   
-    if (f) {
-         int least_max=INT_MAX,index1=index+1;
-        for(int i=index+1;i<A.size();i++){
-          if (A[i] > A[index]) {
-            least_max = min(least_max, A[i]);
-            index1 = (least_max == A[i]) ? i : index1;
-          }
-      }
-    //   cout<<index<<index1<<endl;
-        swap(A[index],A[index1]);
-        sort(A.begin()+index+1,A.end());
-    }
-    else{
-        sort(A.begin(),A.end());
-    }}
-    
 };
