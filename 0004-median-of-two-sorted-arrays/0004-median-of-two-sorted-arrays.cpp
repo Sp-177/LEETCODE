8153f15a72f7ef111e2383e9ea20eebd467d2b01
Shubham Patel <138980314+Sp-177@users.noreply.github.com>
2025-08-06 22:09:23 +0530

    Time: 0 ms (100%), Space: 95 MB (79.23%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,40 +1,31 @@
 class Solution {
 public:
-    double find(vector<int>&a,vector<int>&b){
-        int n=a.size(),m=b.size();
-        int total=n+m;
-        int half=(total+1)>>1;
-        int left=0,right=m;
-        int a1=0,a2=0;
-        while(left<=right){
-            int part=(left+right)>>1;
-            int num_elem_left_first=part;
-            int num_elem_left_second=half-num_elem_left_first;
-            int index2=num_elem_left_second-1;
-            int l1=(part-1>=0)?b[part-1]:INT_MIN,l2=index2>=0?a[index2]:INT_MIN;
-            int r1=(part<m)?b[part]:INT_MAX,r2=(index2+1<n)?a[index2+1]:INT_MAX;
-            // cout<<l1<<"     "<<r1<<endl<<l2<<"    "<<r2<<endl;
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n1=nums1.size(),n2=nums2.size();
+        if(n2>n1)return findMedianSortedArrays(nums2,nums1);
+        int want=(n1+n2)/2;
+        int low=want-n2,high=min(want,n1);
+        int l=-1,r=-1;
+        while(low<=high){
+            int mid=(low+high)>>1;
+            int num1=mid;
+            int num2=want-num1;
+            int ind1=mid-1,ind2=num2-1;
+            // cout<<ind1<<" "<<ind2<<endl;
+            double l1=(ind1<0?INT_MIN:nums1[ind1]);
+            double r1=(ind1+1>=n1?INT_MAX:nums1[ind1+1]);
+            double l2=(ind2<0?INT_MIN:nums2[ind2]);
+            double r2=(ind2+1>=n2?INT_MAX:nums2[ind2+1]);
+            // cout<<l1<<" , "<<r1 <<" "<<l2<<" , "<<r2<<endl;
             if(l1<=r2&&l2<=r1){
-                a1=max(l1,l2);
-                a2=min(r1,r2);
+                l=max(l1,l2);
+                r=min(r1,r2);
                 break;
             }
-            else if(l1>r2){
-                right=part-1;
-            }   
-            else{
-                left=part+1;
-            }
-           
+            if(l1>r2)high=mid-1;
+            else low=mid+1;
         }
-        if(total&1)return a1;
-        return (a1*1.0 +a2*1.0)/(2.0);
-
-    }
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if(nums1.size()<nums2.size()){
-            return find(nums2,nums1);
-        }
-        return find(nums1,nums2);
+        if((n1+n2)&1)return r;
+        return (l+r)/2.0;
     }
 };
