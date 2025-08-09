58d95fa2acd367d9aeb100df5de63d440d803abd
Shubham Patel <138980314+Sp-177@users.noreply.github.com>
2025-08-09 17:22:30 +0530

    Time: 15 ms (20.31%), Space: 20.7 MB (27.77%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,39 +1,38 @@
 class Solution {
 public:
+    int calcArea(vector<int>&h){
+        int n=h.size();
+        stack<int>s1,s2;
+        vector<int>l(n),r(n);
+        for(int i=0;i<n;i++){
+            while(s1.size()&& h[s1.top()]>=h[i])s1.pop();
+            l[i]=(s1.size())?s1.top():-1;
+            s1.push(i);
+        }
+        for(int i=n-1;i>=0;i--){
+            while(s2.size()&& h[s2.top()]>=h[i])s2.pop();
+            r[i]=(s2.size())?s2.top():n;
+            s2.push(i);
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            int left=i-l[i];
+            int right=r[i]-i;
+            int area=(left+right-1)*h[i];
+            ans=max(ans,area);
+        }
+        return ans;
+    }
     int maximalRectangle(vector<vector<char>>& matrix) {
         int n=matrix.size(),m=matrix[0].size();
+        vector<int>vec(m,0);
         int ans=0;
-        vector<int>row(m,0);
-        auto func=[&]()->int{
-            int area=0;
-            // for(int i:row)cout<<i<<" ";
-            // cout<<endl;
-            stack<int>st;
-            for(int i=0;i<=m;i++){
-                int num=i==m?0:row[i];
-                while(st.size() && row[st.top()]>=num){
-                    int ind=st.top();
-                    int h=ind==m?0:row[ind];
-                    st.pop();
-                    int  w=0;
-                    if(st.size()==0)w=i;
-                    else w=i-st.top()-1;
-                    area=max(area,h*w);
-                }
-                st.push(i);
-            }
-            // cout<<area<<endl;
-            return area;
-        };
-    
         for(int i=0;i<n;i++){
-           
             for(int j=0;j<m;j++){
-                if(matrix[i][j]=='0')row[j]=0;
-                else row[j]+=matrix[i][j]-'0';
+                if(matrix[i][j]=='0')vec[j]=0;
+                else vec[j]++;
             }
-             ans=max(ans,func());
-            
+            ans=max(ans,calcArea(vec));
         }
         return ans;
     }
